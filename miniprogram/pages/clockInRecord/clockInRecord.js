5f46e7a06800baeb482a17d4412ecdc75b57e7c6
zhouhoufa <774392980@qq.com>
2020-02-21 16:34:29 +0800

    add clockInRecord page

---
+++ miniprogram/pages/clockInRecord/clockInRecord.js
@@ -0,0 +1,87 @@
+// pages/clockInRecord/clockInRecord.js
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    datas:[
+      {
+        temperature: 37,
+        date: "2020-02-01",
+        status: "无异常症状"
+      },
+      {
+        temperature: 37.2,
+        date: "2020-02-02",
+        status: "腹泻、肌肉酸痛"
+      },
+      {
+        temperature: 37.4,
+        date: "2020-02-03",
+        status: "乏力"
+      },
+      {
+        temperature: 37.1,
+        date: "2020-01-30",
+        status: "无异常症状"
+      }
+    ]
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+
+  }
+})
