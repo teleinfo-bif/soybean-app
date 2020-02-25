7f11b338952bf5295ea431e43a76e03479fd700e
liteng <teng.li@lndl-energy.com>
2020-02-25 16:41:52 +0800

    打卡地点叛空调整

--- miniprogram/pages/healthyClock/healthyClock.js
+++ miniprogram/pages/healthyClock/healthyClock.js
@@ -19,6 +19,7 @@ Page({
       { name: '当地未放行', value: '1' }
     ],
     index: 0,
+    place: "",
     todayClickFlag : '0' //今日是否打卡标志，默认未打卡
   },
 
@@ -213,7 +214,7 @@ Page({
     var isQueZhenFlag = this.isQueZhenFlag
     var goHospitalFlag = this.goHospitalFlag
     var goHBFlag = this.goHBFlag
-    var place = this.place
+    var place = e.detail.value.place
 
     if (name == null || name == '') {
       wx.showToast({
