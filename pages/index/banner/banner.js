6eb30dbed8610e6c9f0744e6cbb86c3d47bede2f
klinsmaya <fzzhang@139.com>
2020-03-02 17:50:33 +0800

    banner找回

---
+++ pages/index/banner/banner.js
@@ -0,0 +1,23 @@
+// pages/index/Banner/banner.js
+// const app = getApp();
+Component({
+  /**
+   * 组件的属性列表
+   */
+  properties: {
+    statusBarHeight: {
+      type: Number,
+      default: 40
+    }
+  },
+
+  /**
+   * 组件的初始数据
+   */
+  data: {},
+
+  /**
+   * 组件的方法列表
+   */
+  methods: {}
+});
