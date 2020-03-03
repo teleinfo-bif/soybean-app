a30ae2f0bdebb75101a23fbc9135cfcf57ab62ce
newship <slow55flow@gmail.com>
2020-03-03 23:19:46 +0800

    group create page

--- pages/personal/index.js
+++ pages/personal/index.js
@@ -273,18 +273,9 @@ Page({
    */
   onUnload: function() {},
 
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function() {},
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function() {},
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function() {}
+  createGroup: function (e) {
+      wx.navigateTo({
+        url: '/pages/group/createGroup/createGroup'
+      })
+  },
 });
