a1073e77946d492307aa79fa0bc854a59c5e8b7f
wangshuai <wangshuai1992@163.com>
2020-03-04 23:22:26 +0800

    refactor(健康码): 健康码跳转之前验证今日打卡状态

--- pages/index/funcBtn/index.js
+++ pages/index/funcBtn/index.js
@@ -1,40 +1,60 @@
 // pages/index/FuncBtn/FuncBtn.js
 //获取应用实例
+function getyyyyMMdd(date) {
+  var d = date || new Date();
+  var curr_date = d.getDate();
+  var curr_month = d.getMonth() + 1;
+  var curr_year = d.getFullYear();
+  String(curr_month).length < 2 ? (curr_month = "0" + curr_month) : curr_month;
+  String(curr_date).length < 2 ? (curr_date = "0" + curr_date) : curr_date;
+  var yyyyMMdd = curr_year + "-" + curr_month + "-" + curr_date;
+  return yyyyMMdd;
+}
 const app = getApp();
+import { getTodayClock } from "../../../api/api";
 
 Component({
+  lifetimes: {
+    attached() {
+      if (!app.globalData.appInit) {
+        app.init(() => {
+          // this.getTodayClockData();
+        });
+      } else {
+        // this.getTodayClockData();
+      }
+    }
+  },
   /**
    * 组件的属性列表
    */
   properties: {
     btnData: {
       type: Object,
-      default: () => { }
+      default: () => {}
     }
-  },
-  attached() {
-    // console.log(app);
   },
 
   /**
    * 组件的初始数据
    */
-  data: {},
+  data: {
+    clocked: null
+  },
 
   /**
    * 组件的方法列表
    */
   methods: {
-    toNextPage(event) {
-      console.log(app);
-      // 如果已经关闭了权限，或者录入过个人信息
-      if (!app.globalData.auth || app.globalData.userRegisted) {
-        var $this = this;
-        // console.log('已录个人信息')
-        wx.navigateTo({
-          url: event.currentTarget.dataset.path
-        });
-      } else {
+    async toNextPage(e) {
+      // 确定APP初始化结束之后，能正常获取注册信息之后再调用
+      if (!app.globalData.appInit) {
+        app.init(this.toNextPage);
+        return;
+      }
+      const { data, path } = e.currentTarget.dataset;
+      // 如果用户未注册
+      if (!app.globalData.userRegisted) {
         wx.showModal({
           title: "温馨提示",
           content: "请您继续完善身份信息",
@@ -48,13 +68,49 @@ Component({
             }
           }
         });
+      } else if (data.permission && !this.data.clocked) {
+        // 存在未打卡false和未请求两种情况不单独验证
+        const clocked = await this.getTodayClockData();
+        if (clocked) {
+          wx.navigateTo({
+            url: e.currentTarget.dataset.path
+          });
+        } else {
+          wx.showModal({
+            title: "温馨提示",
+            content: "您今天还未填写健康情况，请先填写今天的健康情况。",
+            showCancel: false,
+            confirmText: "去完善",
+            success(res) {
+              if (res.confirm) {
+                wx.navigateTo({
+                  url: "/pages/clock/index"
+                });
+              }
+            }
+          });
+        }
+      } else {
+        wx.navigateTo({
+          url: path
+        });
       }
     },
-    clearStorage() {
-      wx.clearStorage();
-      wx.showToast({
-        title: "clearStorage success"
-      });
+    // 获取今日打卡信息
+    async getTodayClockData() {
+      return getTodayClock({
+        time: getyyyyMMdd()
+      })
+        .then(resData => {
+          const locked = resData.total > 0;
+          this.setData({
+            locked
+          });
+          return locked;
+        })
+        .catch(e => {
+          return false;
+        });
     }
   }
 });
