2c98990eb24a540bfa6bd6b641c94aad568c60df
zhouhoufa <774392980@qq.com>
2020-03-04 12:05:07 +0800

    update un commit cloud functions

---
+++ cloudfunctions/unHealthyAuthority/index.js
@@ -0,0 +1,29 @@
+// 云函数入口文件
+const cloud = require('wx-server-sdk')
+
+cloud.init()
+
+const db = cloud.database({ env: "soybean-uat" })
+const _ = db.command
+
+// 云函数入口函数
+exports.main = async (event, context) => {
+  const wxContext = cloud.getWXContext()
+
+  let list = await db.collection('user_healthy').where(_.or([{
+    "bodyStatusFlag": "1"
+  }, {
+    "bodyStatusFlag": "2"
+  }, {
+    'isQueZhenFlag': '0'
+  }]).and([{
+    "date": event.date
+  }, {
+      "company_department": db.RegExp({
+        regexp: event.company_department,
+      })
+  }])).get()
+ 
+
+  return list.data;
+}
