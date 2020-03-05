720c308f2d62f179b81b64ee37aaad6e3e31d733
zhouhoufa <774392980@qq.com>
2020-03-05 16:46:40 +0800

    add level three department sort

--- cloudfunctions/getCompany/index.js
+++ cloudfunctions/getCompany/index.js
@@ -3,7 +3,7 @@ const cloud = require('wx-server-sdk')
 
 cloud.init()
 
-const db = cloud.database({ env: "soybean-uat"})
+const db = cloud.database({ env: "soybean-uat" })
 // const db = cloud.database()   // 这个会报错
 
 async function getCompanyCountIndex() {
@@ -33,7 +33,26 @@ exports.main = async (event, context) => {
 
   // return count;
 
-  let list = await db.collection('company_info').orderBy('weight', 'asc').get({
-  });
+
+  let list = await db.collection('company_info').orderBy('weight', 'asc').get();
+
+  for (let index = 0; index < list.data.length; index ++) {
+    let wantedDepartments = []
+    let departments = list.data[index].departments
+
+    if (departments.length > 0) {
+      wantedDepartments.push("领导")
+    }
+  
+    for (let indexj = 0; indexj < departments.length; indexj ++) {
+      if (departments[indexj] != "领导") {
+        wantedDepartments.push(departments[indexj])
+      }
+    }
+
+    list.data[index].departments = wantedDepartments
+
+  }
+
   return list.data;
 }
