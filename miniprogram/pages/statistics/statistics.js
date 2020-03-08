b887d0cfa48741c337b5715b0bdf433e2e825656
zhouhoufa <774392980@qq.com>
2020-03-08 14:48:22 +0800

    add show excel feature

--- miniprogram/pages/statistics/statistics.js
+++ miniprogram/pages/statistics/statistics.js
@@ -1107,14 +1107,40 @@ parseDatas: function(datas) {
 
   exportExcel: function () {
     let that = this
-    // console.log("get date is ", date)
-    wx.showLoading({
-      title: '数据加载中',
-    })
+
     let currentDate = that.data.showDate
     if (currentDate == undefined) {
       currentDate = that.getCurrentDay()
     }
+
+    let data = {
+      currentDate: currentDate
+    }
+    that.getExcel(data, that.setClipboard)
+  },
+
+  showExcel: function () {
+    let that = this
+    let data = {}
+    that.getExcel(data, that.openExcel)
+  },
+
+  emailExcel: function () {
+    let that = this
+    let data = {
+      fromAddress: "774392980@qq.com",
+      toAddress: "774392980@qq.com",
+      subject: "打卡记录"
+    }
+
+    that.getExcel(data, that.sendEmail)
+  },
+
+  getExcel: function (dataObject, action) {
+    let that = this
+    wx.showLoading({
+      title: '数据加载中',
+    })
 
     wx.cloud.callFunction({
       name: "export",
@@ -1132,30 +1158,9 @@ parseDatas: function(datas) {
           fileList: [res.result.fileID],
           success: res => {
             console.log("res.fileList ", res.fileList)
+            
+            action(dataObject, res)
 
-            wx.setClipboardData({
-              data: res.fileList[0].tempFileURL,
-              success: function (res) {
-                wx.showToast({
-                  icon: 'none',
-                  title: currentDate + "导出文件下载链接已保存到您的剪贴板"
-                });
-              }
-            })
-
-            // this.sendEmail({
-            //   fromAddress: "774392980@qq.com",
-            //   toAddress: "774392980@qq.com",
-            //   subject: "打卡记录",
-            //   content: res.fileList[0].tempFileURL,
-            // })
-
-            // this.sendEmail({
-            //   fromAddress: "774392980@qq.com",
-            //   toAddress: "zzjj64@163.com",
-            //   subject: "打卡记录",
-            //   content: res.fileList[0].tempFileURL,
-            // })
           }
         })
       },
@@ -1166,7 +1171,35 @@ parseDatas: function(datas) {
     })
   },
 
-  sendEmail: function (email) {
+  setClipboard: function (data, res) {
+    wx.setClipboardData({
+      data: res.fileList[0].tempFileURL,
+      success: function (res) {
+        wx.showToast({
+          icon: 'none',
+          title: data.currentDate + "导出文件下载链接已保存到您的剪贴板"
+        });
+      }
+    })
+  },
+
+  openExcel: function (data, res) {
+    wx.downloadFile({
+      url: res.fileList[0].tempFileURL,
+      success: function (res) {
+        var filePath = res.tempFilePath
+        wx.openDocument({
+          filePath: filePath,
+          fileType: "xlsx",
+          success: function (res) {
+            console.log('打开文档成功')
+          }
+        })
+      }
+    })
+  },
+
+  sendEmail: function (email, res) {
     console.log("email ", email)
     wx.cloud.callFunction({
       name: "sendEmail",
@@ -1174,7 +1207,7 @@ parseDatas: function(datas) {
         fromAddress: email.fromAddress,
         toAddress: email.toAddress,
         subject: email.subject,
-        content: email.content,
+        content: res.fileList[0].tempFileURL,
       },
       success: res => {
         console.log("sendEmail", res)
