6f2dd8b557748ce523d362447f05e2d8a9195940
wangshuai <wangshuai1992@163.com>
2020-03-09 13:16:09 +0800

    feat(uploadfile封住): 基于wx.uploadFile封装一次promise写法

---
+++ api/upload.js
@@ -0,0 +1,20 @@
+// ipfs server地址
+const url = "https://admin.bidspace.cn/bid-blockchain/front/ipfs/upload-photo";
+
+export const UploadFile = async ({ filePath, name, formData, ...others }) => {
+  return new Promise((resolve, reject) => {
+    wx.uploadFile({
+      url, //仅为示例，非真实的接口地址
+      filePath,
+      name,
+      formData,
+      ...others,
+      success(res) {
+        resolve(res.data);
+      },
+      fail(error) {
+        reject(error);
+      }
+    });
+  });
+};
