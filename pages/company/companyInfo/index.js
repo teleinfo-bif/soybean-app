90b54aef0a899e5b876cbbf7210f691c9c12e612
amActiveHello <2461015133@qq.com>
2020-03-13 16:55:24 +0800

    细节优化

--- pages/company/companyInfo/index.js
+++ pages/company/companyInfo/index.js
@@ -41,7 +41,7 @@ Page({
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-    
+    options.groupId = 1
     console.log("======groupId==", options.groupId)
     console.log("======permision==", options.permision)
     this.setData({
