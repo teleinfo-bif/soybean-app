ee42224c16d16e75b3f6d4be0930acd5d32518e6
newship <slow55flow@gmail.com>
2020-03-15 15:02:52 +0800

    加入机构选择页面

--- pages/group/shareJoinChoice/index.js
+++ pages/group/shareJoinChoice/index.js
@@ -10,14 +10,6 @@ Page({
     userFilledInfo: "",
     groupId: "",
     groupName: "",
-    multiArray: [
-      ["工业互联网与物联网研究所", "安全研究所", "泰尔系统实验室"],
-      ["技术研究部", "系统开发部", "综合管理部"]
-    ],
-    multiIndex: [0, 0],
-    array: ["技术研究部"],
-    index: 0,
-    lastClass: true,
     alreadJoin: false, //是否已加过群
     alreadJoinId: ""
   },
@@ -27,6 +19,12 @@ Page({
    */
   onLoad: function(options) {
     const { groupId, groupName, alreadJoin, alreadJoinId } = options;
+    console.log(wx.getSystemInfoSync())
+    let windowHeight = wx.getSystemInfoSync().windowHeight // 屏幕的高度
+    let windowWidth = wx.getSystemInfoSync().windowWidth
+    this.setData({
+      scroll_height: windowHeight * 750 / windowWidth - 188
+    })
     app.globalData.join = null;
     if (!app.globalData.appInit) {
       app.init(globalData => {
@@ -55,7 +53,7 @@ Page({
   userPrompt: function() {
     const { groupId, userFilledInfo } = this.data;
     if (userFilledInfo.userRegisted) {
-      this.tree2array(groupId);
+      this.treeUserArray(groupId);
     } else {
       wx.navigateTo({
         url: "/pages/index/index",
@@ -65,99 +63,7 @@ Page({
       });
     }
   },
-  //只适合三级架构模型
-  tree2array: function(groupId) {
-    getUserTreeGroup({
-      groupId: groupId
-    }).then(data => {
-      // console.log('dd', data)
-      if (data.length == 0) {
-        //最底层部门
-        this.setData({
-          lowestClass: true
-        });
-      } else {
-        let a = [{ name: "请选择", id: 0 }];
-        let b = [];
-        data.map((val, index) => {
-          a.push({ name: val.name, id: val.id });
-          b.push(
-            val.children.length == 0
-              ? []
-              : val.children.map(val =>
-                  Object.assign({}, { name: val.name, id: val.id })
-                )
-          );
-        });
-        // console.log(a, b)
-        let lastClass = b.every((val, index) => val.length == 0); //是否倒数第二级
-        this.setData({
-          lastClass: lastClass
-        });
-        if (lastClass) {
-          let array = a;
-          console.log("lastClass", array);
-          this.setData({
-            array: array,
-            joinGroupId: array[0].id
-          });
-        } else {
-          b.unshift([]);
-          let multiArray = [a, b[0]];
-          console.log(multiArray, b);
-          this.setData({
-            first: a,
-            second: b,
-            multiArray: multiArray,
-            joinGroupId: b[0].length == 0 ? a[0].id : b[0][0].id
-          });
-        }
-      }
-    });
-  },
-  bindPickerChange: function(e) {
-    const { array } = this.data;
-    console.log(
-      "picker发送选择改变，携带值为",
-      e,
-      e.detail.value,
-      e.target.dataset.id
-    );
-    let index = e.detail.value;
-    // let id = e.target.dataset.id //这个值有问题
-    let id = array[index].id;
-    this.setData({
-      index: e.detail.value,
-      joinGroupId: id
-    });
-  },
-  bindMultiPickerChange: function(e) {
-    console.log(
-      "picker发送选择改变，携带值为",
-      e.detail.value,
-      e.target.dataset.id
-    );
-    let id = e.target.dataset.id;
-    this.setData({
-      multiIndex: e.detail.value,
-      joinGroupId: id
-    });
-  },
-  bindMultiPickerColumnChange: function(e) {
-    console.log("修改的列为", e.detail.column, "，值为", e.detail.value);
-    var data = {
-      multiArray: this.data.multiArray,
-      multiIndex: this.data.multiIndex
-    };
-    data.multiArray[0] = this.data.first;
-    data.multiIndex[e.detail.column] = e.detail.value;
-    switch (e.detail.column) {
-      case 0:
-        data.multiArray[1] = this.data.second[data.multiIndex[0]];
-        break;
-    }
-    this.setData(data);
-  },
+ 
   join: function() {
     const { joinGroupId, userFilledInfo } = this.data;
     let groupId = joinGroupId;
@@ -213,9 +119,11 @@ Page({
               success: result => {},
               fail: () => {},
               complete: () => {//此处不要用reLanch，防止页面闪动
-                wx.navigateTo({
-                  url: "/pages/index/index"
-                });
+                setTimeout(function () {
+                  wx.navigateTo({
+                    url: "/pages/index/index"
+                  });
+                }, 1500)                
               }
             });
           })
@@ -230,9 +138,11 @@ Page({
             duration: 1500,
             mask: false,
             success: result => {//此处不要用reLanch，防止页面闪动
-              wx.navigateTo({
-                url: "/pages/index/index"
-              });
+              setTimeout(function () {
+                wx.navigateTo({
+                  url: "/pages/index/index"
+                });
+              }, 1500)
             }
           });
         })
@@ -243,6 +153,24 @@ Page({
       //   })
       // }, 2000)
     }
+  },
+
+  //获取数据
+  treeUserArray: function (groupId) {
+    getUserTreeGroup({
+      groupId: groupId
+    }).then(data => {
+      this.setData({
+          companyArray:data
+        })
+    })
+  },
+  selThis(e) {
+    console.log('=====chooseItem===',e.detail);
+    this.setData({
+      chooseItem: e.detail,
+      joinGroupId: e.detail.id
+    })
   },
   /**
    * 生命周期函数--监听页面初次渲染完成
